73c4a11355f498bacf4feeca7a7f6a115f1090e0
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-31 17:53:40 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 if game.PlaceId == 2092166489 then
 
-local GameName = "Area 51 :) - by nexer"
+local GameName = "Area 51 - by nexer"
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "Area 51", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+local Window = OrionLib:MakeWindow({IntroText = "Area 51 - by nexer", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
 
 OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
 
@@ -29,7 +29,8 @@ local Tab1 = Window:MakeTab({
 Tab1:AddButton({
     Name = "Get AK-47",
     Callback = function()
-fireproximityprompt(workspace.Weapons["AK-47"].Hitbox.ProximityPrompt.Parent)
+firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), game.Workspace.Weapons["AK-47"].Hitbox.TouchInterest.Parent, 0)
+firetouchinterest(game.Players.LocalPlayer.Character:WaitForChild("Head"), game.Workspace.Weapons["AK-47"].Hitbox.TouchInterest.Parent, 1)
     end
 })
 			
